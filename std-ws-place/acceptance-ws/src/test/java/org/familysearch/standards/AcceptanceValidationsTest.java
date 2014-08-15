7f75b76ea117fe58966d3ffc7077505726796132
juanjomen <juanjomen@familysearch.org>
2014-08-15 11:21:36 -0600

    all in place to add tests to acceptance test

---
+++ std-ws-place/acceptance-ws/src/test/java/org/familysearch/standards/AcceptanceValidationsTest.java
@@ -0,0 +1,55 @@
+package org.familysearch.standards;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+/**
+ * User:
+ * Date: 5/21/12
+ * Time: 7:46 AM
+ * To validate the Acceptance Module.
+ * NOTE: This is to test the helper methods only.  No need to test the tests that test the code.
+ */
+public class AcceptanceValidationsTest {
+  private AcceptanceValidations fixture = new AcceptanceValidations();
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+
+  }
+
+  @AfterMethod
+  public void tearDown() throws Exception {
+
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void initEmpty() {
+    fixture.init("");
+  }
+
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void initNull(){
+    fixture.init(null);
+  }
+
+  @Test
+  public void initValid(){
+    fixture.init("http://myUrl");
+  }
+
+
+  @Test
+  public void setup()throws Exception{
+    fixture.setUp();
+  }
+
+  @Test
+  public void tearDownTest()throws Exception{
+    fixture.tearDown();
+  }
+}
